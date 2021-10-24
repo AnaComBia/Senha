45eff64f5d585659ed85cacce007a7931647c03f
AnaComBia <anabeatrizetorres@gmail.com>
2021-10-24 19:44:38 -0300

    finaliza programa Senha

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---Senha---\n");
+
+            Console.Write("Digite a senha do usuário: ");
+            String senha = Console.ReadLine();
+
+            if (senha == "123abc")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nAcesso permitido\n");
+                Console.ResetColor();
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nAcesso negado\n");
+                Console.ResetColor();
+            }
+
+
+        }
+    }
+}
